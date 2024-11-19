682b1db4d5c3aa217715c594533f529144f39971
Emmanuel Jean Pierre Auguste <ejp.auguste@ext.soprasteria.com>
2024-11-19 13:15:15 +0100

    Agrego de exception y clases utiles II

--- src/main/java/com/bcnc/pricingapi/controller/PriceController.java
+++ src/main/java/com/bcnc/pricingapi/controller/PriceController.java
@@ -1,8 +1,12 @@
 package com.bcnc.pricingapi.controller;
 
+import com.bcnc.pricingapi.exceptions.DataNotFoundException;
 import com.bcnc.pricingapi.service.PriceService;
 import com.bcnc.pricingapi.dto.PriceResponseDTO;
+import com.bcnc.pricingapi.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +20,19 @@ public class PriceController {
     PriceService priceService;
 
     @GetMapping("/price")
-    public PriceResponseDTO getPrice(@RequestParam int productId, @RequestParam Long brandId,
-                                     @RequestParam String applicationDate) {
+    public ResponseEntity<PriceResponseDTO> getPrice(@RequestParam int productId, @RequestParam Long brandId,
+                                                     @RequestParam String applicationDate) {
         LocalDateTime date = LocalDateTime.parse(applicationDate);
+        PriceResponseDTO results = priceService.getApplicablePrice(productId, brandId, date);
 
-        return priceService.getApplicablePrice(productId, brandId, date);
+        boolean allFieldsNull = ObjectUtils.areAllFieldsNull(results);
+
+        if (allFieldsNull)
+        {
+            throw new DataNotFoundException(HttpStatus.NOT_FOUND,"DatabaseNotFound",
+                    "Datos no encontrados para la fecha indicada" );
+        }
+        return ResponseEntity.ok(results);
     }
 
 }
